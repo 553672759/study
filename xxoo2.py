c251451c96b1bca6fadf86612030ed4e4de022e5
553672759 <553672759@qq.com>
2017-05-26 16:50:45 +0800

    2017年5月26日

--- xxoo2.py
+++ xxoo2.py
@@ -1,31 +1,49 @@
-#coding:utf8
+# -*- coding:utf-8 -*-  
+'''
+Created on 2017年5月24日
 
-import wx
-class MyApp(wx.App):
-    def OnInit(self):
-        #frame=wx.Frame(None,title="Hello,python")
-        #frame.Show()
-        return True
+@author: xx
+'''
+import urllib2
+import cookielib
+import random
+import socket
+
+url="https://btso.pw/search/sis"
+
+class BrowserBase(object): 
     
-#带有输入框的Frame
-class Frame1(wx.Frame):
-    def __init__(self,superior):
-        wx.Frame.__init__(self,parent=superior,title="hello",pos=(400,200),size=(700,400))
-        self.panel=wx.Panel(self)
-        #text1=wx.TextCtrl(panel,value="hello,python",size=(200,100))
-        self.panel.Bind(wx.EVT_LEFT_UP,self.OnClick)
-    #当鼠标左键点击弹起时发生的事件    
-    def OnClick(self,event):
-        posm=event.GetPosition()
-        wx.StaticText(parent=self.panel,label="hello",pos=(posm.x,posm.y))
+    def __init__(self):
+        socket.setdefaulttimeout(20)
+        self.page= ''
+        self.articleName = ''
+        self.link = ''
+      
+    def speak(self,name,content):
+        print '[%s]%s' %(name,content)
+    
+    def getpage(self,url):
+        cookie_support=urllib2.HTTPCookieProcessor(cookielib.CookieJar())
+        self.opener = urllib2.build_opener(cookie_support,urllib2.HTTPHandler)
+        urllib2.install_opener(self.opener)
+        user_agents=['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36']
+        self.opener.addheaders=[("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"),
+                                ("Accept-Language","zh-CN,zh;q=0.8"),
+                                ("Host","btso.pw"),
+                                ("User-Agent",user_agents)
+                                ]
+        try:
+            response=self.opener.open(url)
+            return response.read()
+            
+        except Exception,e:
+            self.speak(str(e),url)
+            raise Exception
+        else:
+            return response
         
-if __name__=='__main__':
-    app=MyApp()
-    frame=Frame1(None)
-    frame.Show(True)
-    app.MainLoop()
-    
-
-
-
+        
+if __name__=="__main__":
+    splider=BrowserBase()
+    page=splider.openurl("https://btso.pw/search/sis")
 
